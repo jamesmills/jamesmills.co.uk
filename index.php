07f360a29ccea5a7a738da6aeff5f1867bfa035a
James Mills <jamesmills@Jamess-MacBook-Pro.local>
2014-11-10 08:26:56 +0000

    Initial copy from original on BitBucket

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8" />
+
+    <title>James Mills</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" lang="en" content="Personal homepage for James Mills." />
+    <meta name="keywords" lang="en" content="james mills, james, mills, jamesmills" />
+    <meta name="robots" content="index,follow" />
+    <meta name="author" content="James Mills" />
+    <meta name="revisit-after" content="3 DAYS" />
+    <meta http-equiv="content-language" content="en" />
+    <meta name="rating" content="general" />
+
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+    <!-- Load jQuery using Google code API -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+    <!-- Custom jQuery stuff -->
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+
+    <!-- Boostrap CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/custom.css" media="screen, projection"/>
+
+    <!-- Load font from Google Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Istok+Web&v2' rel='stylesheet' type='text/css'>
+
+    <!-- Boostrap JS -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
+    <!-- Google Anylitics tracking code -->
+    <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-324842-10']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+    </script>
+
+</head>
+
+<body>
+
+<div class="container">
+
+
+    <div class="jumbotron">
+        <div class="container">
+            <h1>James Mills</h1>
+            <p>
+                PHP developer working for <a href="http://twitter.com/clicksco">Clicksco</a> currently based in Dubai.
+                <br /><br />
+            </p>
+            <p>
+                <a class="btn btn-default" href="http://blog.jamesmills.co.uk">Personal Blog</a>
+                <a class="btn btn-default" href="http://live.jamesmills.co.uk">Live Timeline</a>
+            </p>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-12">
+
+            <ul class="social">
+                <li><a href="http://twitter.com/jamesmills" class="icon-twitter"></a></li>
+                <li><a href="https://github.com/jamesmills" class="icon-github"></a></li>
+                <li><a href="http://facebook.com/jamesgmills" class="icon-facebook"></a></li>
+                <li><a href="http://linkedin.com/in/jamesgmills" class="icon-linkedin"></a></li>
+            </ul>
+         </div>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-3 further-info">
+            <img src="assets/img/logos/live_refresh.png">
+            <div class="further-info-link-block">
+                <a href="http://www.refreshteeside.org" class="btn btn-primary">Website</a>
+                <a href="http://twitter.com/refreshteesside" class="btn btn-info">Twitter</a>
+            </div>
+            <h3>Refresh Teesside</h3>
+            <p>Refresh Teesside is a FREE event for people in the creative industry in and around Teesside. The aim is to encourage collaboration and help promote businesses in the region. </p><p>We meet on the second Thursday of every month at Sassari's, Middlesbrough from 6pm onwards.</p>
+
+        </div>
+
+        <div class="col-md-3 further-info">
+            <img src="assets/img/logos/live_bramhope.png">
+            <div class="further-info-link-block">
+                <a href="http://www.bramhopebears.co.uk" class="btn btn-primary">Website</a>
+                <a href="http://twitter.com/bramhopebears" class="btn btn-info">Twitter</a>
+            </div>
+            <h3>Bramhope Bears</h3>
+            <p>Bramhope Bears was previously known as Bears4Everyone. We had a lovely family run shop in Otley, West Yorkshire. As a family we decided that in order to give our bears, and our customers, the attention they deserve we should focus solely on our website. Therefore, we closed the door on Bears4Everyone and have launched our new website “Bramhope Bears”.</p>
+        </div>
+
+        <div class="col-md-3 further-info">
+            <img src="assets/img/logos/live_myshopassist.png">
+            <div class="further-info-link-block">
+                <a href="http://www.myshoppingassistant.co.uk" class="btn btn-primary">Website</a>
+                <a href="http://twitter.com/myshopassist" class="btn btn-info">Twitter</a>
+            </div>
+            <h3>My Shopping Assistant</h3>
+            <p>Imagine doing your online shopping, and being able to add all the items you need for a meal to your tesco.com basket with just one click! My Shopping Assistant allows you to do that. Simply create ‘packages’ for all your regular items on My Shopping Assistant and use these to make online shopping as quick and easy as possible.</p>
+        </div>
+
+        <div class="col-md-3 further-info">
+            <img src="assets/img/logos/live_jgmwd.png">
+            <div class="further-info-link-block">
+                <a href="http://www.jgmwebdesign.co.uk" class="btn btn-primary">Website</a>
+                <a href="http://twitter.com/jgmwebdesign" class="btn btn-info">Twitter</a>
+            </div>
+            <h3>JGM Web Design Ltd</h3>
+            <p>I do consultancy and development work on a Friday.</p>
+        </div>
+
+    </div>
+
+</div><!-- /container -->
+
+</body>
+</html>
